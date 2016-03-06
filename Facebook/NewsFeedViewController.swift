732bcde802f4f3f03105d5efa8c3c14651339ce8
Jenn Leung <jleung@jleung.local>
2016-03-06 13:26:17 -0800

    clean up

--- Facebook/NewsFeedViewController.swift
+++ Facebook/NewsFeedViewController.swift
@@ -20,9 +20,7 @@ class NewsFeedViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         lightboxTransition = LightboxTransition()
-        //lightboxTransition.heyQT = "what"
         NewsFeedScrollView.contentSize = homeFeedImageView.image!.size
-        //print (photoArray)
     }
     
     override func didReceiveMemoryWarning() {
@@ -60,60 +58,12 @@ class NewsFeedViewController: UIViewController {
 
     
         photoViewController.weddingImageIndex = photoArray.indexOf(selectedImageView)
-        //print(photoArray[selectedImageView.image.index+1])
-        //trying to grab index of array and so i could show previous and next photos
         print("0 indexed : \(photoArray[0])")
         print(photoArray.indexOf(selectedImageView))
         lightboxTransition.selectedImageCenter = selectedImageView.center
-        //print(selectedImageView.center)
         
         photoViewController.weddingImages = photoArray
         
     }
-    
-    
-    
-    
-    
-    //    func animationControllerForPresentedController(presented: UIViewController!, presentingController presenting: UIViewController!, sourceController source: UIViewController!) -> UIViewControllerAnimatedTransitioning! {
-    //        isPresenting = true
-    //        return self
-    //    }
-    //
-    //    func animationControllerForDismissedController(dismissed: UIViewController!) -> UIViewControllerAnimatedTransitioning! {
-    //        isPresenting = false
-    //        return self
-    //    }
-    //    func transitionDuration(transitionContext: UIViewControllerContextTransitioning) -> NSTimeInterval {
-    //        // The value here should be the duration of the animations scheduled in the animationTransition method
-    //        return 0.4
-    //    }
-    
-    //    func animateTransition(transitionContext: UIViewControllerContextTransitioning) {
-    //        print("animating transition")
-    //        let containerView = transitionContext.containerView()!
-    //        let toViewController = transitionContext.viewControllerForKey(UITransitionContextToViewControllerKey)!
-    //        let fromViewController = transitionContext.viewControllerForKey(UITransitionContextFromViewControllerKey)!
-    //
-    //        if (isPresenting) {
-    //            containerView.addSubview(toViewController.view)
-    //            toViewController.view.alpha = 0
-    //            UIView.animateWithDuration(0.4, animations: { () -> Void in
-    //                toViewController.view.alpha = 1
-    //                }) { (finished: Bool) -> Void in
-    //                    transitionContext.completeTransition(true)
-    //            }
-    //        } else {
-    //            UIView.animateWithDuration(0.4, animations: { () -> Void in
-    //                fromViewController.view.alpha = 0
-    //                }) { (finished: Bool) -> Void in
-    //                    transitionContext.completeTransition(true)
-    //                    fromViewController.view.removeFromSuperview()
-    //            }
-    //        }
-    //    }
-    
-    
-    
     
 }
